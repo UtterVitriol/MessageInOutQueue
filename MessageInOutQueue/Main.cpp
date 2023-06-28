9a94eaf6f489633e3da093b9762cd9b494441e9c
UtterVitriol <uttervitriol@gmail.com>
2023-06-28 19:57:29 -0400

    stole a thread pool

--- MessageInOutQueue/Main.cpp
+++ MessageInOutQueue/Main.cpp
@@ -13,27 +13,15 @@ void test(MessageQueue *que)
 	que->QueueIn(ree);
 }
 
+struct job
+{
+	int tid;
+	int cchunk;
+	int tchunk;
+	void* msg;
+};
 
 int main()
 {
-	MessageQueue que{};
-	Message* ree = MessageFactory(23);
-
-	Message* in = MessageFactory(10);
-	Message* out = MessageFactory(20);
-
-	que.QueueIn(in);
-	que.QueueOut(out);
-
-	Message* temp = que.WaitForMessage();
-
-	printf("Temp: %d\n", temp->GetItem());
-
-	temp = que.WaitForMessage();
-	printf("Temp: %d\n", temp->GetItem());
-	std::thread b = std::thread(test, &que);
-
-	temp = que.WaitForMessage();
-	printf("End Temp: %d\n", temp->GetItem());
-	b.join();
+	
 }
